b96dfe03bae8f88b79cb0ce0a276da351c481710
Justin <me@justinle.page>
2021-10-02 22:33:54 -0700

    InfoStrip initial

---
+++ src/components/InfoStrip/index.js
@@ -0,0 +1,15 @@
+import { Container, Row } from 'react-bootstrap';
+import React from 'react';
+
+export default function InfoStrip() {
+    return (
+        <div className="my-5">
+            <Container>
+                <Row className="text-center">
+                    <h1>Our Services</h1>
+                    <h6>Test</h6>
+                </Row>
+            </Container>
+        </div>
+    );
+}
