0cd9b67a8e480f27b14a1f577c879c01f0646bb3
Justin <me@justinle.page>
2021-10-04 15:45:29 -0700

    sticky nav and navbar brand

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,8 @@ import { Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import React from 'react';
+import Logo from '../../assets/logo_simple.png';
+import './style.css';
 
 export default function Header() {
     const navItems = [
@@ -20,9 +22,19 @@ export default function Header() {
     ];
 
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="light" variant="light" className="sticky-nav">
             <Container>
-                <Navbar.Brand href="#home">Top Notch LV</Navbar.Brand>
+                <Navbar.Brand href="#home" className="d-flex">
+                    {' '}
+                    <img
+                        alt="top-notch-lv-logo"
+                        src={Logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    <div className="px-3">Top Notch LV</div>
+                </Navbar.Brand>
                 <Nav className="justify-content-between">
                     {navItems.map((page, index) => (
                         <Nav.Link key={`nav-link-${index}`} href={page.url}>
