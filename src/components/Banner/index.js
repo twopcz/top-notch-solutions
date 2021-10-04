958ce22c57e32074eff3b6c81f32c9fe4c99cf2a
Justin <me@justinle.page>
2021-10-04 15:45:50 -0700

    padding botton

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Banner() {
     return (
-        <Row className="w-100 mx-0">
+        <Row className="w-100 mx-0 pb-5">
             <img className="px-0" src={banner} alt="banner" />
         </Row>
     );
