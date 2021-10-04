426e93fd3d3ae6b2af399e07c66107b1b44e3ecd
Justin <me@justinle.page>
2021-10-04 16:48:45 -0700

    added Pricing

--- src/components/InfoStrip/index.js
+++ src/components/InfoStrip/index.js
@@ -73,6 +73,44 @@ export default function InfoStrip() {
                     </p>
                 </Col>
             </Row>
+            <Row className="text-center py-5">
+                <h1 className="display-4">Pricing</h1>
+            </Row>
+            <Row className="pb-5">
+                <Col>
+                    <h5>PC Repair</h5>
+                    <h6 className="card-subtitle text-muted my-1">
+                        $99 flat rate
+                    </h6>
+                    <p>
+                        Flat rate for a single computer. Includes, but not
+                        limited to: data back up, operating system install,
+                        driver updates, software install (if provided), etc.
+                    </p>
+                </Col>
+                <Col>
+                    <h5>On Site Services</h5>
+                    <h6 className="card-subtitle text-muted my-1">
+                        $100 per hour
+                    </h6>
+                    <p>
+                        Includes, but not limited to: consulting, hardware
+                        installations, software installations, upgrades, back
+                        ups, managements of technology, etc.
+                    </p>
+                </Col>
+                <Col>
+                    <h5>Business Rate</h5>
+                    <h6 className="card-subtitle text-muted my-1">
+                        $66-$80 per hour
+                    </h6>
+                    <p>
+                        Our hourly labor, provided at a discount, if Top Notch
+                        Computer Solutions is contracted to be your on call tech
+                        solution source.
+                    </p>
+                </Col>
+            </Row>
         </Container>
     );
 }
