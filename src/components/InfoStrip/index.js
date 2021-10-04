44844dd689c1374687f9744e80d5ca8432d89929
Justin <me@justinle.page>
2021-10-04 15:47:15 -0700

    infostrip update

--- src/components/InfoStrip/index.js
+++ src/components/InfoStrip/index.js
@@ -7,74 +7,72 @@ import React from 'react';
 
 export default function InfoStrip() {
     return (
-        <div className="my-5">
-            <Container>
-                <Row className="text-center py-5">
-                    <h1 className="display-4">Our Services</h1>
-                    <p className="lead">
-                        We have a team of experts that cover a wide variety of
-                        tech solutions
+        <Container className="my-5">
+            <Row className="text-center py-5">
+                <h1 className="display-4">Our Services</h1>
+                <p className="lead">
+                    We have a team of experts that cover a wide variety of tech
+                    solutions
+                </p>
+            </Row>
+            <Row className="text-center">
+                <Col>
+                    <h5>Computers</h5>
+                    <img
+                        className="img-fluid py-3"
+                        src={Board}
+                        alt="computer-board"
+                    ></img>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim. sed do eiusmod
+                        tempor incididunt..
                     </p>
-                </Row>
-                <Row className="text-center">
-                    <Col>
-                        <h5>Computers</h5>
-                        <img
-                            className="img-fluid py-3"
-                            src={Board}
-                            alt="computer-board"
-                        ></img>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim. sed do
-                            eiusmod tempor incididunt..
-                        </p>
-                    </Col>
-                    <Col>
-                        <h5>Server Maintenance</h5>
-                        <img
-                            className="img-fluid py-3"
-                            src={Server}
-                            alt="server-rack"
-                        ></img>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim. sed do
-                            eiusmod tempor incididunt..
-                        </p>
-                    </Col>
-                    <Col>
-                        <h5>Multifunction Printers</h5>
-                        <img
-                            className="img-fluid  py-3"
-                            src={Printer}
-                            alt="printer"
-                        ></img>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim. sed do
-                            eiusmod tempor incididunt..
-                        </p>
-                    </Col>
-                    <Col>
-                        <h5>& more!</h5>
-                        <img
-                            className="img-fluid  py-3"
-                            src={Phones}
-                            alt="phones"
-                        ></img>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim. sed do
-                            eiusmod tempor incididunt..
-                        </p>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+                </Col>
+                <Col>
+                    <h5>Server Maintenance</h5>
+                    <img
+                        className="img-fluid py-3"
+                        src={Server}
+                        alt="server-rack"
+                    ></img>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim. sed do eiusmod
+                        tempor incididunt..
+                    </p>
+                </Col>
+                <Col>
+                    <h5>Multifunction Printers</h5>
+                    <img
+                        className="img-fluid  py-3"
+                        src={Printer}
+                        alt="printer"
+                    ></img>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim. sed do eiusmod
+                        tempor incididunt..
+                    </p>
+                </Col>
+                <Col>
+                    <h5>& more!</h5>
+                    <img
+                        className="img-fluid  py-3"
+                        src={Phones}
+                        alt="phones"
+                    ></img>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim. sed do eiusmod
+                        tempor incididunt..
+                    </p>
+                </Col>
+            </Row>
+        </Container>
     );
 }
