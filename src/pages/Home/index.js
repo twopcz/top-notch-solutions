5d6709833049f068acb1f54209a1c4b712277dca
Justin <me@justinle.page>
2021-10-04 15:53:45 -0700

    homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,18 @@
 import Header from '../../components/Header';
+import Banner from '../../components/Banner';
+import Footer from '../../components/Footer';
+import InfoStrip from '../../components/InfoStrip';
+import Contact from '../../components/Contact';
 import React from 'react';
 
 export default function Home() {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <Banner />
+            <InfoStrip />
+            <Contact />
+            <Footer />
+        </>
+    );
 }
